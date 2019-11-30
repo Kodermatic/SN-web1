2fb24ccae7b16a476d627859950a07f3c94185d3
Kodermatic <56317944+Kodermatic@users.noreply.github.com>
2019-11-30 13:39:55 +0100

    Updated

--- 09_HW - Python (Functions)/game_functions.py
+++ 09_HW - Python (Functions)/game_functions.py
@@ -33,12 +33,27 @@ def top_scores(f_path, f_filename, f_no_of_top_scores, f_sorted_by="attemps"):  
   n = 1                                                                             #  - put top results in variable
   f_top_scores = ""
   for i in f_sorted_lists:
-    f_top_scores += str(n) + ".)" + "   attemps:" + str(i["attemps"]) + "   date:" + str(i["date"]) + "\n"  # do not use comma instead +
+    f_top_scores += str(n) + ".)" + "   attemps:" + str(i["attemps"]) + "   secret:" + str(i["secret"]) + "   level:" + str(i["level"]) + "   date:" + str(i["date"]) + "\n"  # do not use comma instead +
     if n >= f_no_of_top_scores:
       break
     n += 1
   
+  f_top_scores = f"\n Top {n-1} scores are: \n" + f_top_scores
   return f_top_scores
+
+def choose_level():
+  while True:
+    level = input("Please select level of game. \n press 1 for 'easy' or press 2 for 'hard': ")
+    if level in ["1", "2"]:
+      if level == "1":
+        level = "easy"
+        break
+      elif level == "2":
+        level = "hard"
+        break
+    else:
+      print("You have entered invalid value.")
+  return level 
 
 def play_game():  #Function that launch 1 cycle of number guessing game
   attemps = 0
@@ -50,28 +65,32 @@ def play_game():  #Function that launch 1 cycle of number guessing game
     if guess == secret:
       print(f"You have guessed it! Congratulation! It is {secret}.\n You did it in {attemps} attemp/s!")
       score_list = read_scores(my_path, my_filename)
-      attemp_data = {"attemps": attemps, "date": str(datetime.datetime.now())}
+      attemp_data = {"attemps": attemps, "secret": secret, "level": my_level, "date": str(datetime.datetime.now())}
       score_list.insert(0, attemp_data)
       write_scores(my_path, my_filename, score_list)
       break
     
     elif guess > secret:
-      print("You guess is not correct try something smaller.")
-      
-    elif guess < secret:
-      print("You guess is not correct try something bigger.")
+      if my_level == "easy":
+        print("You guess is not correct try something smaller.")
+      else:
+        print("You guess is not correct try something different.")
     
+    elif guess < secret:
+      if my_level == "easy":
+        print("You guess is not correct try something bigger.")
+      else:
+        print("You guess is not correct try something different.") 
     else:
       print("You guess entry is invalid.")
 
 
 #### Program procedure
 create_file(my_path, my_filename, "[]")   #creates score file if it does not exists
+my_level = choose_level()
 play_again = "y"
 while play_again == "y":
   play_game()
   play_again = input("Input 'y' if you want to play again or anything for exit: ").lower()
-  
-print(f"\n Top {my_no_of_top_scores} scores are:")
 print(top_scores(my_path, my_filename, my_no_of_top_scores))
 print("Thank you for playing!")
