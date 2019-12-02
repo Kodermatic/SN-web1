71ca7ec5b809ce6814826098c2c7cf9c2dd6fe83
Kodermatic <56317944+Kodermatic@users.noreply.github.com>
2019-12-02 20:29:45 +0100

    Code Best Practice Update

--- 09_HW - Python (Functions)/game_functions.py
+++ 09_HW - Python (Functions)/game_functions.py
@@ -5,27 +5,28 @@ import datetime
 from os import path
 
 ### General parameters settings
-my_path = "/Users/x/PycharmProjects/GIT/SN-web1/09_HW - Python (Functions)/"
+my_path = "./09_HW - Python (Functions)"
 my_filename = "f-scorelist.txt"
+my_path_filename = path.join(my_path, my_filename)
 my_no_of_top_scores = 5
 
 ### Functions
-def write_scores(f_path, f_filename, f_list):               # Function to write f_list variable into file
-  with open(f_path + f_filename, "w") as f_score_file:
+def write_scores(f_path_filename, f_list):               # Function to write f_list variable into file
+  with open(f_path_filename, "w") as f_score_file:
      json.dump(f_list, f_score_file)
  
-def read_scores(f_path, f_filename):
-  with open(f_path + f_filename, "r") as f_score_file:      # Function to read file and load it into variable 
-    f_score_list = json.load(f_score_file)                  # and return variable with file content
+def read_scores(f_path_filename):
+  with open(f_path_filename, "r") as f_score_file:       # Function to read file and load it into variable 
+    f_score_list = json.load(f_score_file)               # and return variable with file content
     return f_score_list
 
-def create_file(f_path, f_filename, f_content):             # Create empty file if does not exist
-  if path.exists(f_path + f_filename) == False:
-    with open(f_path + f_filename, "w") as f_score_file:
+def create_file(f_path_filename, f_content):             # Create empty file if does not exist
+  if path.exists(f_path_filename) == False:
+    with open(f_path_filename, "w") as f_score_file:
       f_score_file.write(f_content)
 
-def top_scores(f_path, f_filename, f_no_of_top_scores, f_sorted_by="attemps"):      # Function print top x results
-  with open(f_path + f_filename, "r") as f_score_file:                              # - read file and load it into variable 
+def top_scores(f_path_filename, f_no_of_top_scores, f_sorted_by="attemps"):      # Function print top x results
+  with open(f_path_filename, "r") as f_score_file:                               # - read file and load it into variable 
     f_score_list = json.load(f_score_file)               
   
   f_sorted_lists = sorted(f_score_list, key=lambda k: k[f_sorted_by])               #  - sort list of dictionaries by f_sorted_by variable
@@ -33,12 +34,12 @@ def top_scores(f_path, f_filename, f_no_of_top_scores, f_sorted_by="attemps"):  
   n = 1                                                                             #  - put top results in variable
   f_top_scores = ""
   for i in f_sorted_lists:
-    f_top_scores += str(n) + ".)" + "   attemps:" + str(i["attemps"]) + "   secret:" + str(i["secret"]) + "   level:" + str(i["level"]) + "   date:" + str(i["date"]) + "\n"  # do not use comma instead +
+    f_top_scores = f"{n}.)    attemps: {i['attemps']}    secret: {i['secret']}    level: {i['level']}   date: {i['date']} \n"
     if n >= f_no_of_top_scores:
       break
     n += 1
   
-  f_top_scores = f"\n Top {n-1} scores are: \n" + f_top_scores
+  f_top_scores = f"\n Top {n-1} scores are: \n {f_top_scores}"
   return f_top_scores
 
 def choose_level():
@@ -64,10 +65,10 @@ def play_game():  #Function that launch 1 cycle of number guessing game
   
     if guess == secret:
       print(f"You have guessed it! Congratulation! It is {secret}.\n You did it in {attemps} attemp/s!")
-      score_list = read_scores(my_path, my_filename)
+      score_list = read_scores(my_path_filename)
       attemp_data = {"attemps": attemps, "secret": secret, "level": my_level, "date": str(datetime.datetime.now())}
       score_list.insert(0, attemp_data)
-      write_scores(my_path, my_filename, score_list)
+      write_scores(my_path_filename, score_list)
       break
     
     elif guess > secret:
@@ -86,11 +87,11 @@ def play_game():  #Function that launch 1 cycle of number guessing game
 
 
 #### Program procedure
-create_file(my_path, my_filename, "[]")   #creates score file if it does not exists
+create_file(my_path_filename, "[]")   #creates score file if it does not exists
 my_level = choose_level()
 play_again = "y"
 while play_again == "y":
   play_game()
   play_again = input("Input 'y' if you want to play again or anything for exit: ").lower()
-print(top_scores(my_path, my_filename, my_no_of_top_scores))
+print(top_scores(my_path_filename, my_no_of_top_scores))
 print("Thank you for playing!")
